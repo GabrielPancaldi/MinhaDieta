08ebfa65a8600801157f2b40fe5556eee02f8fe3
Gabriel Pancaldi <pancaldig@hotmail.com>
2023-05-30 20:47:16 -0300

    adicionar express e mustache

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-var teste;
+var express = require('express');
+var indexRouter = require('./routes/index');
+var path = require('path');
+
+const app = express();
+
+// view engine setup
+var mustacheExpress = require("mustache-express");
+var engine = mustacheExpress();
+app.engine("mustache", engine);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
+
+app.use(express.json());
+app.use('/', indexRouter);
+
+
+
+app.listen(3000, () => {
+    console.log("Listenning...")
+})
+
+
+module.exports = app;
+
+
