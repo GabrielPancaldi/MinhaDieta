93609415d88e64151b8b7608e8fb0c457b0b35ba
Gabriel Pancaldi <pancldig@hotmail.com>
2023-06-01 20:31:46 -0300

    ajuste views e css

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,31 @@ var path = require('path');
 
 router.get('/', (req, res) => {
 
+    res.render("index");
+
+});
+
+router.get('/contato', (req, res) => {
+
+    res.render("contato");
+
+});
+
+router.get('/tecnologia', (req, res) => {
+
+    res.render("tecnologia");
+
+});
+
+router.get('/sobre', (req, res) => {
+
     res.render("sobre");
+
+});
+
+router.get('/desenvolvedor', (req, res) => {
+
+    res.render("desenvolvedor");
 
 });
 
